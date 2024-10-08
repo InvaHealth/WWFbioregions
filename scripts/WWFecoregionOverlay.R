e01f378a2e5accc0fc5f778b03c710535c11070c
Corey Bradshaw <cjabradshaw@gmail.com>
2024-10-08 11:26:34 +0200

    Add files via upload

---
+++ scripts/WWFecoregionOverlay.R
@@ -0,0 +1,37 @@
+library(terra)
+
+# coordinate example
+lat.ex <- 43.608979250502436
+lon.ex <- 3.878053035338895
+pts <- vect(cbind(lon.ex, lat.ex), crs="+proj=longlat")
+
+# WWF ecoregion polygon
+setwd("~/Downloads/WWFbioregions")
+WWFecoregions <- vect("wwf_terr_ecos.shp") 
+WWFecoregions
+head(WWFecoregions['BIOME'])
+table(WWFecoregions$BIOME)
+
+# biome name key (https://databasin.org/datasets/68635d7c77f1475f9b6c1d1dbe0a4c4c/)
+biome.names <- c('Tropical & Subtropical Moist Broadleaf Forests',
+                 'Tropical & Subtropical Dry Broadleaf Forests',
+                 'Tropical & Subtropical Coniferous Forests',
+                 'Temperate Broadleaf & Mixed Forests',
+                 'Temperate Conifer Forests',
+                 'Boreal Forests/Taiga',
+                 'Tropical & Subtropical Grasslands, Savannas & Shrublands',
+                 'Temperate Grasslands, Savannas & Shrublands',
+                 'Flooded Grasslands & Savannas',
+                 'Montane Grasslands & Shrublands',
+                 'Tundra',
+                 'Mediterranean Forests, Woodlands & Scrub',
+                 'Deserts & Xeric Shrublands',
+                 'Mangroves')
+biome.key <- data.frame('value'=seq(1,14,1), 'name'=biome.names)
+
+# plot
+terra::plot(WWFecoregions, 'BIOME')
+
+#and extract
+ecoregpts <- terra::extract(WWFecoregions, pts)
+biome.key$name[which(biome.key$value == ecoregpts$BIOME)]
